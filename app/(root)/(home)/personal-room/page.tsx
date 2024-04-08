508151c62b415e171f87045d89615074be2b245e
Allan <allanrs19@gmail.com>
2024-04-07 21:47:56 -0400

    fixed meetingLink in personal-room

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -18,7 +18,7 @@ const PersonalRoom = () => {
 
   const { user } = useUser();
   const meetingId = user?.id;
-  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
+  const meetingLink = `https://${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meetingId}?personal=true`;
 
   const { toast } = useToast();
 
