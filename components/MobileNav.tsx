6c6ddb74f0c39a053ac273e73d6465583a84bd40
Allan <allanrs19@gmail.com>
2025-01-23 07:41:57 -0400

    Fixed mobile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -40,7 +40,7 @@ const MobileNav = () => {
                             alt="Yoom Logo"
                             className="max-sm:size-10"
                         />
-                        <p className="text-[26px] font-extrabold text-white">Rashelle bb</p>
+                        <p className="text-[26px] font-extrabold text-white">MeetUp</p>
                     </Link>
 
                     <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
